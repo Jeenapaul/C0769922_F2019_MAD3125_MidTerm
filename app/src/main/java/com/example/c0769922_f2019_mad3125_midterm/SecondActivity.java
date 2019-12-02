088329926b36f5a398653bb315627a82825fab6c
Jeenapaul <jeenapaul1014@gmail.com>
2019-12-02 18:29:18 -0500

    Create variable for SecondActivity class

--- app/src/main/java/com/example/c0769922_f2019_mad3125_midterm/SecondActivity.java
+++ app/src/main/java/com/example/c0769922_f2019_mad3125_midterm/SecondActivity.java
@@ -3,8 +3,13 @@ package com.example.c0769922_f2019_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView fName;
+    TextView Age;
+    TextView taxfilingdate;
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
