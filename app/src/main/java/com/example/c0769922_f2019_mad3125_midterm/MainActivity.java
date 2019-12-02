67871efc5ca2ea86b346aa3f1468138bf1601cd4
jeenapaul <jeenapaul1014@gmail.com>
2019-12-02 16:17:55 -0500

    declaring fields in mainactivity

--- app/src/main/java/com/example/c0769922_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0769922_f2019_mad3125_midterm/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.c0769922_f2019_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText sin;
+
+        EditText firstName;
+
+        EditText lastName;
+
+        EditText birth;
+
+        EditText income;
+
+        EditText RRSP;
+
+        RadioGroup genderGroup;
+
+        RadioButton radioGenderButton;
+
+        Calendar myCalendar = Calendar.getInstance();
+
+        private Calendar calendar;
+
+        int day, month, year;
+
+        private DatePickerDialog datePickerDialog;
+
+        private int remaining;
     }
     //test
 }
